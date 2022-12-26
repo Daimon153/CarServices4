c481ba65922f0e0cb0b73895e26c533921aacc3f
Vara1on1 <lemillion13@gmail.com>
2022-12-26 09:26:14 +0500

    Добавьте файлы проекта.

---
+++ CarServices/Model/PartialClass/ServicePartial.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CarServices.Model
+{
+    public partial class Service
+    {
+        public string DiscountText
+        {
+            get
+            {
+                if (Discount == 0 || Discount == null)
+                    return "";
+                else
+                    return $"* скидка {Discount * 100} %";
+            }
+        }
+        public string FullPath
+        {
+            get
+            {
+                 return $"/{ServicePhoto.PhotoPath}";
+            }
+        }
+        public double CostWithDiscount
+        {
+            get
+            {
+                if (Discount == 0 || Discount == null)
+                {
+                    return (double)Cost;
+                }
+                else
+                {
+                    var costWithDiscount = (double)Cost * (1.00 - Discount);
+                    return costWithDiscount.Value;
+                }
+            }
+        }
+        public string TotalCost
+        {
+            get
+            {
+                if (Discount == 0 || Discount == null)
+                {
+                    return $"{Cost:N2} рублей за {Convert.ToString(DurationInSeconds / 60)} минут";
+                }
+                else
+                {
+                    //Convert.ToInt32(DurationInSeconds)
+                    return $"{CostWithDiscount:N2} рублей за {Convert.ToString(DurationInSeconds /60)} минут";
+                }
+            }
+        }
+        public Visibility DiscountVisibility
+        {
+            get
+            {
+                if (Discount == 0 || Discount == null)
+                    return Visibility.Collapsed;
+                else
+                    return Visibility.Visible;
+            }
+        }
+        public string BackColor
+        {
+            get
+            {
+                if (Discount == 0 || Discount == null)
+                    return "#FFFFE1";
+                else
+                    return "#D1FFD1";
+            }
+        }
+
+    }
+}
---
+++ CarServices/View/Pages/ServicePage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using CarServices.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarServices.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ServicesPage.xaml
+    /// </summary>
+    public partial class ServicePage : Page
+    {
+        Core db = new Core();
+        List<Service> arrayService;
+        public ServicePage()
+        {
+            InitializeComponent();
+            arrayService = db.context.Services.ToList();
+            LViewServices.ItemsSource = arrayService;
+            ComboDiscount.SelectedIndex = 0;
+            ComboSortBy.SelectedIndex = 0;
+        }
+
+        private void BtnAddService_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboSortBy_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+        private void UpdateServices()
+        {
+            var services = db.context.Services.ToList();
+
+            if (ComboSortBy.SelectedIndex == 0)
+                services = services.OrderBy(p => p.CostWithDiscount).ToList();
+            else
+                services = services.OrderByDescending(P => P.CostWithDiscount).ToList();
+            if (ComboSortBy.SelectedIndex == 1)
+                services = services.OrderBy(p => p.Discount >= 0 && p.Discount < 0.05 ).ToList();
+            if (ComboSortBy.SelectedIndex == 2)
+                services = services.OrderBy(p => p.Discount >= 0.05 && p.Discount < 0.15).ToList();
+            if (ComboSortBy.SelectedIndex == 3)
+                services = services.OrderBy(p => p.Discount >= 0.15 && p.Discount < 0.30).ToList();
+            if (ComboSortBy.SelectedIndex == 4)
+                services = services.OrderBy(p => p.Discount >= 0.30 && p.Discount < 0.70).ToList();
+            if (ComboSortBy.SelectedIndex == 5)
+                services = services.OrderBy(p => p.Discount >= 0.70 && p.Discount < 1).ToList();
+
+            services = services.Where(p => p.Title.ToLower().Contains(TBoxsearch.Text.ToLower())).ToList();
+
+            LViewServices.ItemsSource = services;
+        }
+
+        private void ComboDiscount_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void TBoxsearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LViewServices_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
